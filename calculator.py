24732438ed28f117d578e0c3e69f5e06f36a7152
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 12:39:52 -0700

    finished basic program

--- calculator.py
+++ calculator.py
@@ -14,10 +14,27 @@ while True:
     tokens = input.split(" ")
     operator = tokens[0]
     num1 = int(tokens[1])
-    num2 = int(tokens[2])
+    num2 = int(tokens[-1])
+    
     if operator == "q":
         break
     elif operator == "+":
-        print(add(num1, num2))
+        print add(num1, num2)
     elif operator == "-":
-        print(subtract(num1, num2))
+        print subtract(num1, num2)
+    elif operator == "*":
+        print multiply(num1, num2)
+    elif operator == "/":
+        print divide(num1, num2)
+    elif operator == "square":
+        print square(num1)
+    elif operator == "cube":
+        print cube(num1)
+    elif operator == "power":
+        print power(num1, num2)
+    elif operator == "mod":
+        print mod(num1, num2)
+    else:
+        print "I don't understand"
+
+
